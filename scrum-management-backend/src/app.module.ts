ffae1610043388fac57d9277347e5dabcd12e5d0
Shreya Ghosh <sgshreya024@gmail.com>
2023-11-27 20:22:37 +0530

    Mysql database created

--- scrum-management-backend/src/app.module.ts
+++ scrum-management-backend/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterModule } from './modules/register/register.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRegister } from './modules/register/model/user-register';
+
 
 @Module({
-  imports: [RegisterModule],
+  imports: [RegisterModule,TypeOrmModule.forRoot({
+    type:"mysql",
+    host:"localhost",
+    port:3306,
+    username:"s3",
+    password:"testpassword",
+    database:"scrum_management",
+    entities:[UserRegister],
+    synchronize:true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
--- scrum-management-backend/src/modules/register/model/user-register.ts
+++ scrum-management-backend/src/modules/register/model/user-register.ts
@@ -1,5 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class UserRegister{
+    @PrimaryGeneratedColumn({type:'int'})
+        id:number;
+    
+    @Column()
     name:string;
+
+    @Column()
     email:string;
+
+    @Column()
     phone:string;
+
+    @Column()
+    password:string;
+
+    @Column()
+    type:string;
 }
